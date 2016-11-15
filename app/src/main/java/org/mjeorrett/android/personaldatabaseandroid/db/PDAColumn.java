c77f44bbbe4ffb7cc7ad9e67f94ed4e2f6282d91
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-11-15 15:44:24 +0000

    adding basic implementation

---
+++ app/src/main/java/org/mjeorrett/android/personaldatabaseandroid/db/PDAColumn.java
@@ -0,0 +1,43 @@
+package org.mjeorrett.android.personaldatabaseandroid.db;
+
+import android.content.Intent;
+
+import java.util.List;
+
+/**
+ * Created by user on 15/11/2016.
+ */
+
+public class PDAColumn implements PDAEntity {
+
+    private PDATable mTable;
+    private String mName;
+
+    PDAColumn( PDATable table, String name ) {
+
+        mTable = table;
+        mName = name;
+    }
+
+    @Override
+    public String getTitle() {
+
+        return mName;
+    }
+
+    @Override
+    public List<PDAEntity> getChildEntities() {
+        return null;
+    }
+
+    @Override
+    public Intent putExtrasInIntent(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void createNewChildEntity(String title) {
+
+    }
+
+}
